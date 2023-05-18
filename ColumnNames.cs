a88db9d4c1505dcd447c74b28f6df5c62d75cae7
Thomasgetacademy <thomasg@getacademy.no>
2023-05-18 08:50:54 +0200

    Add project files.

---
+++ ColumnNames.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordgåter;
+
+public class ColumnNames
+{
+    private int someNumber;
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Ordgåter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = CreateWordList();
+            int i = 1;
+            do
+            {
+                string randomWord = list[RandomWordIndexMethod(list)];
+                string lastThreeLetters = randomWord.Substring(randomWord.Length - 3);
+
+                string similarWord = list.FirstOrDefault(w => w.Substring(0, 3) == lastThreeLetters) ?? "No words ends with this";
+                Console.WriteLine($"{randomWord} -> {similarWord}");
+                i++;
+            } while (i < 200);
+
+            Console.ReadLine();
+        }
+
+        private static int RandomWordIndexMethod(string[] list)
+        {
+            Random random = new Random();
+            int randomWordIndex = random.Next(0, list.Length);
+            return randomWordIndex;
+        }
+
+        static string[] CreateWordList()
+        {
+            var namesArray = new List<string>();
+            string filePath = @"C:\Users\Thomim\OneDrive\Get academy koding\tema_3\uke_3\Ordgåter\ordliste.txt";
+            string sep = "\t";
+            int size = 20000;
+            string[] ordArray2 = File.ReadAllLines(filePath);
+
+            foreach (var line in ordArray2)
+            {
+                string[] words = line.Split(sep);
+                if (namesArray.Count < size)
+                {
+                    if (words[1].Length is > 6 and < 11 && !namesArray.Contains(words[1]) && !words[1].Contains(' ') &&
+                    !words[1].Contains('-'))
+                    {
+                        namesArray.Add(words[1]);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return namesArray.ToArray();
+
+        }
+    }
+}
+
+
